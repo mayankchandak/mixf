3cf339218b5e14503cb477008db791e078692146
mayankchandak <60479264+mayankchandak@users.noreply.github.com>
2023-03-27 17:42:34 +0530

    .

--- lib/train/data/loader.py
+++ lib/train/data/loader.py
@@ -2,7 +2,8 @@ import torch
 import torch.utils.data.dataloader
 import importlib
 import collections
-from torch._six import string_classes, int_classes
+from torch._six import string_classes
+int_classes = int
 from lib.utils import TensorDict, TensorList
 
 

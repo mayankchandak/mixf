f14b1f0ced128afd259e20427fbb33d622e99eff
mayankchandak <60479264+mayankchandak@users.noreply.github.com>
2023-03-28 17:35:03 +0530

    .

--- lib/test/evaluation/datasets.py
+++ lib/test/evaluation/datasets.py
@@ -7,7 +7,7 @@ DatasetInfo = namedtuple('DatasetInfo', ['module', 'class_name', 'kwargs'])
 pt = "lib.test.evaluation.%sdataset"  # Useful abbreviations to reduce the clutter
 
 dataset_dict = dict(
-    nat2021_test=DatasetInfo(module="nat2021_test", class_name="NATDataset", kwargs=dict()),
+    nat2021_test=DatasetInfo(module="natdataset", class_name="NATDataset", kwargs=dict()),
     otb=DatasetInfo(module=pt % "otb", class_name="OTBDataset", kwargs=dict()),
     nfs=DatasetInfo(module=pt % "nfs", class_name="NFSDataset", kwargs=dict()),
     uav=DatasetInfo(module=pt % "uav", class_name="UAVDataset", kwargs=dict()),

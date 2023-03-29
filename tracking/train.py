5a132c606364ed4fc9c4f8adb02b8c84beffd0fd
mayankchandak <60479264+mayankchandak@users.noreply.github.com>
2023-03-29 16:04:12 +0530

    .

--- tracking/train.py
+++ tracking/train.py
@@ -16,7 +16,6 @@ def parse_args():
                         help="train on single gpu or multiple gpus")
     parser.add_argument('--nproc_per_node', default=0, type=int, help="number of GPUs per node")  # specify when mode is multiple
     parser.add_argument('--master_port', type=int, help="master port", default=26500)
-    parser.add_argument('--use_lmdb', type=int, choices=[0, 1], default=0)  # whether datasets are in lmdb format
     parser.add_argument('--script_prv', type=str, help='training script name')
     parser.add_argument('--config_prv', type=str, default='baseline', help='yaml configure file name')
     # for knowledge distillation
@@ -32,16 +31,10 @@ def parse_args():
 def main():
     args = parse_args()
     if args.mode == "single":
-        train_cmd = "python lib/train/run_training.py --script %s --config %s --save_dir %s --use_lmdb %d " \
+        train_cmd = "python lib/train/run_training.py --script %s --config %s --save_dir %s " \
                     "--script_prv %s --config_prv %s --distill %d --script_teacher %s --config_teacher %s --stage1_model %s" \
-                    % (args.script, args.config, args.save_dir, args.use_lmdb, args.script_prv, args.config_prv,
+                    % (args.script, args.config, args.save_dir, args.script_prv, args.config_prv,
                        args.distill, args.script_teacher, args.config_teacher, args.stage1_model)
-    elif args.mode == "multiple":
-        train_cmd = "python -m torch.distributed.launch --master_port %d --nproc_per_node %d lib/train/run_training.py " \
-                    "--script %s --config %s --save_dir %s --use_lmdb %d --script_prv %s --config_prv %s  " \
-                    "--distill %d --script_teacher %s --config_teacher %s --stage1_model %s" \
-                    % (args.master_port, args.nproc_per_node, args.script, args.config, args.save_dir, args.use_lmdb, args.script_prv,
-                       args.config_prv, args.distill, args.script_teacher, args.config_teacher, args.stage1_model)
     else:
         raise ValueError("mode should be 'single' or 'multiple'.")
     print(train_cmd)

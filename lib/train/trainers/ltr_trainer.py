9f7641f2ab589c9b4dc34edf88ccf723e51f7e08
mayankchandak <60479264+mayankchandak@users.noreply.github.com>
2023-04-09 12:47:06 +0530

    .

--- lib/train/trainers/ltr_trainer.py
+++ lib/train/trainers/ltr_trainer.py
@@ -88,6 +88,7 @@ class LTRTrainer(BaseTrainer):
         dataset_size = min(len(loader), len(self.nat_loader))
         print("Debug | Dataset size :", dataset_size)
         for data_iter_step in range(1, dataset_size + 1):
+            print("Debug 0")
             day_data = next(loader_iter)
             night_data = next(nat_loader_iter)
             # style_data = day_data.detach().clone()

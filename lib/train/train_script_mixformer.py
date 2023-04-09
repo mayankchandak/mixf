6332b2333d9f51de4902b25851297b86a3acb56f
mayankchandak <60479264+mayankchandak@users.noreply.github.com>
2023-04-09 13:12:23 +0530

    .

--- lib/train/train_script_mixformer.py
+++ lib/train/train_script_mixformer.py
@@ -58,7 +58,8 @@ def run(settings):
     if settings.script_name == "mixformer_vit":
         net = build_mixformer_vit(cfg)
         # print("load pretrain")
-        # net.load_state_dict(torch.load('/workspace/Mayank/original-udat/mixformer.pth', map_location='cpu')['net'], strict=False)
+        print("Model loading from:", '/workspace/Mayank/original-udat/mixformer.pth')
+        net.load_state_dict(torch.load('/workspace/Mayank/original-udat/mixformer.pth', map_location='cpu')['net'], strict=False)
         # print("load done")
     else:
         raise ValueError("illegal script name")
